9d01b8409e46f52dc1e598e29f36edebebb83089
Yanis540 <yanistabellout4@gmail.com>
2025-02-28 17:21:13 +0100

    added first example

--- src/test/java/it/unive/lisa/tutorial/ConcreteValueTest.java
+++ src/test/java/it/unive/lisa/tutorial/ConcreteValueTest.java
@@ -1,7 +1,5 @@
 package it.unive.lisa.tutorial;
 
-import org.junit.Test;
-
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
@@ -10,22 +8,22 @@ import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.conf.LiSAConfiguration.GraphType;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
-import it.unive.lisa.program.Program;
-import it.unive.lisa.interprocedural.OpenCallPolicy;
 import it.unive.lisa.interprocedural.ReturnTopPolicy;
+import it.unive.lisa.program.Program;
+import org.junit.Test;
 
-public class IntervalTest {
+public class ConcreteValueTest {
 
 	@Test
 	public void testInterval() throws ParsingException, AnalysisException {
 		// we parse the program to get the CFG representation of the code in it
-		Program program = IMPFrontend.processFile("inputs/signs.imp");
+		Program program = IMPFrontend.processFile("inputs/concretevalue.imp");
 
 		// we build a new configuration for the analysis
 		LiSAConfiguration conf = new DefaultConfiguration();
 
 		// we specify where we want files to be generated
-		conf.workdir = "outputs/interval";
+		conf.workdir = "outputs/concretevalue";
 
 		// we specify the visual format of the analysis results
 		conf.analysisGraphs = GraphType.HTML;
@@ -33,11 +31,12 @@ public class IntervalTest {
 		// we specify the analysis that we want to execute
 		conf.abstractState = DefaultConfiguration.simpleState(
 				DefaultConfiguration.defaultHeapDomain(),
-				new ValueEnvironment<>(new Interval()),
+				new ValueEnvironment<>(ConcreteValue.TOP),
 				DefaultConfiguration.defaultTypeDomain());
 
-		// we instantiate LiSA with our configuration
 		conf.openCallPolicy = ReturnTopPolicy.INSTANCE;
+
+		// we instantiate LiSA with our configuration
 		LiSA lisa = new LiSA(conf);
 
 		// finally, we tell LiSA to analyze the program
